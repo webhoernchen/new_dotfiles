d0d50e723e36a60f64e04137d43f4b823d62ddc5
Christian Eichhorn <ceichhor@laptop.(none)>
2012-06-19 00:27:34 +0200

    * update_externals.sh updated

--- update_externals.sh
+++ update_externals.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 wget https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim -O vim/autoload/pathogen.vim
+
+cd vim/bundle/pyflakes-vim && git submodule init && git submodule update && cd ../../..
+
+wget https://raw.github.com/tomasr/molokai/master/colors/molokai.vim -O vim/colors/molokai.vim
+
+#sudo add-apt-repository ppa:richarvey/nodester
+#sudo apt-get update
+#sudo apt-get install nodejs
+#sudo ln -s /usr/bin/nodejs /usr/local/bin/node
