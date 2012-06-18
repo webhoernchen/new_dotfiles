9dabaff79e7d8177a74c7325ccf0c4e0b4f63dbb
Christian Eichhorn <ceichhor@laptop.(none)>
2012-06-19 00:36:13 +0200

    * coffee.snippets added

--- update_externals.sh
+++ update_externals.sh
@@ -5,6 +5,7 @@ wget https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim -O v
 cd vim/bundle/pyflakes-vim && git submodule init && git submodule update && cd ../../..
 
 wget https://raw.github.com/tomasr/molokai/master/colors/molokai.vim -O vim/colors/molokai.vim
+wget https://raw.github.com/shanejonas/coffeeScript-VIM-Snippets/master/coffee.snippets -O vim/snippets/coffee.snippets
 
 #sudo add-apt-repository ppa:richarvey/nodester
 #sudo apt-get update
